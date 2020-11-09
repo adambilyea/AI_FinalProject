2ebc776192a8c405cf1c5d8a8eac57d89578b386
Ricky <rrcaceres99@gmail.com>
2020-11-09 17:08:32 -0500

    ai finished

---
+++ AI_FinalProject/Assets/AI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public int Team;
+    private int State = 1;
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (State)
+            {
+            //Chase after ball
+            case 1:
+                float step = 3 * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, ball.transform.position, step);
+                //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.0f);
+                //Vector3.Lerp(transform.position, ball.transform.position, Time.time);
+                break;
+            //Defending
+            case 2:
+                break;
+            default:
+                break;
+                
+            }
+
+    }
+}
