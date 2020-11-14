640819e65688a76608318f76c54f8ec1bccec430
Ricky <rrcaceres99@gmail.com>
2020-11-13 19:24:05 -0500

    a

--- AI_FinalProject/Assets/AI.cs
+++ AI_FinalProject/Assets/AI.cs
@@ -7,6 +7,9 @@ public class AI : MonoBehaviour
     public int Team;
     private int State = 1;
     public GameObject ball;
+    private int speed = 5;
+    public int Reward;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +23,7 @@ public class AI : MonoBehaviour
             {
             //Chase after ball
             case 1:
-                float step = 3 * Time.deltaTime;
+                float step = speed * Time.deltaTime;
                 transform.position = Vector3.MoveTowards(transform.position, ball.transform.position, step);
                 //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.0f);
                 //Vector3.Lerp(transform.position, ball.transform.position, Time.time);
